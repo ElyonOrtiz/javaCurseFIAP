bd7ead3d5569d15cfa94228f21cbd953e48704e2
elyon <elyon.ortiz08@gmail.com>
2024-05-13 16:04:30 -0400

    lesson 3 completed

---
+++ src/com/fiap/classes/Conta.java
@@ -0,0 +1,52 @@
+package com.fiap.classes;
+
+public class Conta {
+    private int numero;
+    private int agencia;
+    private double saldo;
+
+    public Conta(){
+
+    }
+    public Conta(int numero, int agencia, double saldo){
+        this.numero = numero;
+        this.agencia = agencia;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    public double verificarSaldo(){
+        return saldo;
+    }
+
+    public void retirar(double valor){
+        saldo = saldo -valor;
+    }
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
---
+++ src/com/fiap/classes/Teste.java
@@ -0,0 +1,13 @@
+package com.fiap.classes;
+
+public class Teste {
+    public static void main(String[] args){
+        Conta cc = new Conta(20, 123,321);
+
+        cc.depositar(100);
+        System.out.println(cc.getSaldo());
+
+        Conta poupança = new Conta(111, 222, 333);
+        System.out.println(poupança.verificarSaldo());
+    }
+}
